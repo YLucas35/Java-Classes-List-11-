3db93e1cd827dbc7d0e5ed2d177d7c0ab4a205ab
YLucas35 <0001244748@senaimgaluno.com.br>
2026-05-08 16:37:42 -0300

    Create _atividade_2.java

---
+++ _atividade_2.java
@@ -0,0 +1,13 @@
+package Java_Lista_11_ForEach_com_Vetor;
+
+public class _atividade_2 {
+    public static void main(String[] args) {
+        String[] c1 = {"São Paulo", "Rio de Janeiro", "Curitiba", "Chicago", "Salvador"};
+
+        System.out.println("Cidades listadas pelo programa:");
+
+        for (String c2 : c1) {
+            System.out.println(c2);
+        }
+    }
+}
